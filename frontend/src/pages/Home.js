b7e33b1ad2e119b4568d29901504dc47a20cdc8b
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-06-16 17:53:20 +0530

    get workouts imp

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,33 @@
+import { useState, useEffect } from "react";
+
+// components
+import WorkoutDetails from "../components/WorkoutsDetails";
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+        console.log(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
     </div>
   );
 };
